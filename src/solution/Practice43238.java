70d9f70de370dea8b2b96ba79e92703507408c6a
unknown <sunnylee3771@gmail.com>
2024-01-07 11:45:03 +0900

    프로그래머스 입국심사를 풀었따

--- src/solution/Practice43238.java
+++ src/solution/Practice43238.java
@@ -25,52 +25,64 @@ public class Practice43238 {
 //        System.out.println(p.solution(n, times));
 
         //solution(6, [6,10]) : 24
-        int n = 6;
-        int[] times = {6,10};
-        System.out.println(p.solution(n, times));
+//        int n = 6;
+//        int[] times = {6,10};
+//        System.out.println(p.solution(n, times));
 
         //solution(6, [8,10]) : 30
 //        int n = 6;
 //        int[] times = {8,10};
+//
 //        System.out.println(p.solution(n, times));
 
         //solution(6, [4,10]) : 20
+//        int n = 6;
+//        int[] times = {4,10};
+//        System.out.println(p.solution(n, times));
+
         //solution(11, [3,4,10]) : 18
-        //solution(5, [1,1,10]) :
+//        int n = 11;
+//        int[] times = {3,4,10};
+//        System.out.println(p.solution(n, times));
+
+        //solution(5, [1,1,10]) : 3
+//        int n = 5;
+//        int[] times = {1,1,10};
+//        System.out.println(p.solution(n, times));
+
+        int n = 7;
+        int[] times = {10,10};
+        System.out.println(p.solution(n, times));
+
     }
 
     public long solution(int n, int[] times) {
         long answer = 0;
         Arrays.sort(times);
         long start = 0;
-        long end = (long)times[times.length-1] * n;
+        long end = (long)times[times.length-1] * (long)n;
         long middle;
+
         while (true) {
-            middle = (start+ end) / 2;
+            long count = 0;
             if(start > end) {
                 break;
             }
-            long count = 0;
-
+            middle = (start+ end) / 2;
 
             for(int j = 0; j <times.length; j++) {
-
+                count += middle/(long)times[j];
             }
 
-
-            if(count == n) {
-                break;
-            }
-
-            if(count > n) {
+            if(count >= n) {
                 end = middle-1;
-            }
+                answer = middle;
 
-            if(count < n) {
+            }else if(count < n) {
                 start = middle+1;
             }
 
         }
-        return middle;
+        return answer;
     }
 }
