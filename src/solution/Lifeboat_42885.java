adfec647173a88ef2664df4f6b38f188ca0f6b42
hyesunlee30 <sunnylee3771@gmail.com>
2024-01-11 17:49:49 +0900

    구조보트 구조하고 싶다

--- src/solution/Lifeboat_42885.java
+++ src/solution/Lifeboat_42885.java
@@ -1,9 +1,7 @@
 package solution;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Lifeboat_42885 {
     public static void main(String[] args) {
@@ -12,24 +10,56 @@ public class Lifeboat_42885 {
 //        int limit = 100;
 //        //3
 //        System.out.println(m.solution(people,limit));
-        int [] people = {70, 80, 50};
+//        int [] people = {70, 80, 50};
+//        int limit = 100;
+//        //3
+//        System.out.println(m.solution(people,limit));
+        int [] people = {20,30,40,30,70, 80, 50};
         int limit = 100;
         //3
         System.out.println(m.solution(people,limit));
+
     }
     public int solution(int[] people, int limit) {
 
-        int answer = people.length;
-        Arrays.sort(people);
+        int answer = 0;
+//        //오름차순 선착장에 남아 있는 사람
+//        Arrays.sort(people);
+//        List<Integer> sortedPeople = Arrays.stream(people).boxed().collect(Collectors.toList());
+        //내림차순 먼저 태울 사람
+        List<Integer> reversSortedPeople = Arrays.stream(people).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
 
-        Queue queue = new LinkedList();
-        for (int i = 0; i < people.length; i++) {
+        List<Queue<Integer>> boatList = new ArrayList<>();
+        int idx = 0;
+        while (!reversSortedPeople.isEmpty()){
+            PriorityQueue<Integer> boat = new PriorityQueue<>();
+            boat.add(reversSortedPeople.get(idx)); //먼저 태우기
+            System.out.println(" "+reversSortedPeople.get(idx));
+
+            int remainLimit = limit - boat.peek(); // 첫번쨰 탑승자 타고 남은 키로
+            System.out.println("remainLimit "+remainLimit);
+            List<Queue<Integer>> remainarr = new ArrayList<>();
+
+            boolean[] visited = new boolean[reversSortedPeople.size()];
+            visited[idx] = true;
+            for (int i = 0; i < reversSortedPeople.size(); i++) {
+                if(remainLimit >= reversSortedPeople.get(i) && !visited[i]) {
+                    visited[i] = true;
+                }
+            }
+            reversSortedPeople.remove(idx); // 0번째 삭제
 
 
-            queue.add(people[i]);
+            boatList.add(boat);
+            System.out.println("remainarr "+boatList);
+            for (Integer i : boat) {
+                reversSortedPeople.remove(i);
+            }
 
 
         }
+
+        answer = boatList.size();
         return answer;
     }
 }
