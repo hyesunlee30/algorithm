1cc403a23bb601e19839cb4bb160259f06affdf8
hyesunlee30 <sunnylee3771@gmail.com>
2024-01-12 17:37:37 +0900

    구조구조

--- src/solution/Lifeboat_42885.java
+++ src/solution/Lifeboat_42885.java
@@ -7,16 +7,13 @@ import java.util.stream.Collectors;
 public class Lifeboat_42885 {
     public static void main(String[] args) {
         Lifeboat_42885 m = new Lifeboat_42885();
-        //100 - 큰값 빼고 남은 값을 서칭해야한다
+        //제일 무거운 사람이 제일 가벼운 사람과 타지 못하면
+        //혼자 타야함, 2명밖에 못 탄다.
 //        int [] people = {70, 50, 80, 50};
 //        int limit = 100;
 //        //3
 //        System.out.println(m.solution(people,limit));
-//        int [] people = {70, 80, 50};
-//        int limit = 100;
-//        //3
-//        System.out.println(m.solution(people,limit));
-        int [] people = {20,30,40,30,70, 80, 50};
+        int [] people = {70, 80, 50};
         int limit = 100;
         //3
         System.out.println(m.solution(people,limit));
@@ -28,21 +25,36 @@ public class Lifeboat_42885 {
 //        //오름차순 선착장에 남아 있는 사람
 //        Arrays.sort(people);
 //        List<Integer> sortedPeople = Arrays.stream(people).boxed().collect(Collectors.toList());
-        //내림차순 먼저 태울 사람
+        //내림차순 먼저 태울 무거운 사람
         List<Integer> reversSortedPeople = Arrays.stream(people).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
 
 
-        List<Integer> boat = new ArrayList<>();
-        int sum = 0;
-        for (int i =0; i< people.length; i++) {
-            boat.add(people[i]);
-            if(i != 0) {
+        List<List<Integer>> boatList = new ArrayList<>();
 
 
+        for (int i =0; i< reversSortedPeople.size(); i++) {
+            List<Integer> boat = new ArrayList<>();
+
+            int remain = limit - reversSortedPeople.get(i);
+            boat.add(reversSortedPeople.get(i));
+            reversSortedPeople.remove(i);
+
+            int idx = reversSortedPeople.size()-1;
+            int person = reversSortedPeople.get(idx);
+
+            if(remain == person) {
+                boat.add(reversSortedPeople.get(idx));
+                System.out.println(reversSortedPeople.remove(idx));
+            } else if (remain < person && idx == 0) {
+                boat.add(reversSortedPeople.get(idx));
             }
+            System.out.println(boat);
 
+            boatList.add(boat);
+            System.out.println(" ##"+reversSortedPeople);
+            System.out.println("person "+person);
         }
 
-        return answer;
+        return boatList.size();
     }
 }
