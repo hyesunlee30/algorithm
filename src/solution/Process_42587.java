ce37e6857eaabafd233a37b9ef8fc93f42fafe5e
unknown <sunnylee3771@gmail.com>
2024-01-18 21:54:23 +0900

    프로세스 풀어보자

---
+++ src/solution/Process_42587.java
@@ -0,0 +1,17 @@
+package solution;
+
+public class Process_42587 {
+    public static void main(String[] args) {
+        //https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+        Process_42587 s = new Process_42587();
+
+        int[] priorities = {2, 1, 3, 2};
+        int location = 2 ;
+    }
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+}
