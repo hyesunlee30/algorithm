840ec2d22db2c2f6081c7143e3f1397677620de5
hyesunlee30 <sunnylee3771@gmail.com>
2024-01-23 14:03:14 +0900

    주시가격

---
+++ src/solution/StockPrice_42584.java
@@ -0,0 +1,20 @@
+package solution;
+
+public class StockPrice_42584 {
+    public static void main(String[] args) {
+        //https://school.programmers.co.kr/learn/courses/30/lessons/42584
+        //가격이 떨어지지 않은 기간
+        StockPrice_42584 c = new StockPrice_42584();
+        int[] prices = {1, 2, 3, 2, 3};
+        c.solution(prices);
+    }
+
+    public int[] solution(int[] prices) {
+
+
+        int[] answer = {};
+
+        return answer;
+    }
+
+}
